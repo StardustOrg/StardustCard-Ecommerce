48d28adb92d89af5c53068cdc987702f05e281df
yanna-torres <torres.yanna03@gmail.com>
2023-11-22 16:50:59 -0300

    cleaning unsed depedencies

--- src/java/controller/shoppingcart/AddProductShoppingCartServlet.java
+++ src/java/controller/shoppingcart/AddProductShoppingCartServlet.java
@@ -1,13 +1,7 @@
 package controller.shoppingcart;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.URLEncoder;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,9 +12,6 @@ import model.shoppingcart.ShoppingCart;
  * @author Yanna
  */
 public class AddProductShoppingCartServlet extends HttpServlet {
-
-    private static final Type CART_TYPE = new TypeToken<ShoppingCart>() {}.getType();
-    private static final Gson gson = new Gson();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
